6a3aebceace5c3853615ef07904f4cb1b9cc035c
Cafehdidar <rezaxm80@atomicmail.io>
2026-06-10 05:39:33 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // این دوتا بخش پایین به گیت‌هاب می‌گن گیر نده و سایت رو بساز:
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
